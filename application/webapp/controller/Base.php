e1dd38307a98c3979eb4be3f52e986f3793b8ec6
Mezz <mz_vip@qq.com>
2019-08-07 13:27:02 +0800

    debug

--- application/webapp/controller/Base.php
+++ application/webapp/controller/Base.php
@@ -8,5 +8,11 @@ class Base extends Controller
 	public function __construct()
 	{
 		parent::__construct();
+
+		// 允许发起的跨域请求
+		$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '*';
+		header("Access-Control-Allow-Origin: " . $origin);
+		header("Access-Control-Allow-Credentials : true"); 
+		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie");
 	}
 }
